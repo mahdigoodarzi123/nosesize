bb930ef861d1060f013ea5678f23be9c57b9bed9
mahdi goodarzi <73882488+mahdigoodarzi123@users.noreply.github.com>
2022-04-01 00:07:34 +0430

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
+import random
+
+
+
+
+token = "5224103068:AAFu7LRamMkLs706ngV5OYsU-iaYa3IB8Dc"
+updater = Updater(token , use_context=True)
+
+nosesize = ["voldermort" , "small" , "large" , " XL" , '2XL' , "elephant"]
+
+def start(update: Update, context: CallbackContext):
+    size = random.choice(nosesize)
+    (update.message.reply_text("you are " + size))
+
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('nosesize', start))
+  
+updater.start_polling()
+
